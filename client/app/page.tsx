fd4c6268e48b1cc4818d5816ae434e4c24e2838a
N.I <iworksWeb@gmail.com>
2025-03-12 23:41:33 +0900

    client add fetch getAllPosts()

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+import { getAllPosts } from "@/utils/api";
+
+export default async function Home() {
+  const posts = await getAllPosts();
+  console.log(posts);
+
   return (
     <div>
       <h1>Hello World</h1>
