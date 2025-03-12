52707f862393b07672c2c34ffaabfac6dccd2121
N.I <iworksWeb@gmail.com>
2025-03-12 23:23:11 +0900

    add endpoint POST http://localhost:5000/posts

--- server/src/posts/posts.controller.ts
+++ server/src/posts/posts.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { PostType } from './post.interface';
 
 // http://localhost:5000/posts
 @Controller('posts')
@@ -9,5 +10,11 @@ export class PostsController {
   @Get()
   findAll() {
     return this.postsService.findAll();
+  }
+
+  // @NOTE: テストデータ server/json/post_data.json に配置
+  @Post()
+  create(@Body() post: PostType): void {
+    this.postsService.create(post);
   }
 }
