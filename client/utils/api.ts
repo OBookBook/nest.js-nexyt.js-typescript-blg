b6cd4078f5735f82b5344f18564c9641d3bd74f9
N.I <iworksWeb@gmail.com>
2025-03-13 00:29:24 +0900

    client add endpoint POST:http:localhost:5000/posts

--- client/utils/api.ts
+++ client/utils/api.ts
@@ -1,3 +1,5 @@
+import { PostType } from "@/types";
+
 export async function getAllPosts() {
   const response = await fetch("http:localhost:5000/posts", {
     method: "GET",
@@ -11,6 +13,24 @@ export async function getPostById(id: string) {
   const response = await fetch(`http:localhost:5000/posts/${id}`, {
     method: "GET",
   });
+  const data = await response.json();
+
+  return data;
+}
+
+export async function createPost(post: PostType) {
+  const response = await fetch("http:localhost:5000/posts", {
+    method: "POST",
+    body: JSON.stringify(post),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("投稿の作成に失敗しました");
+  }
+
   const data = await response.json();
 
   return data;
